e43be82b8240d7e5962429a44bcfc2a70ed7d51e
Mazabrard Corentin <corentin.mazabrard@epitech.eu>
2024-05-11 18:27:57 +0200

    fix: ajout info pour build web

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,13 @@ void main() async {
   await Firebase.initializeApp(
     options: const FirebaseOptions(
       apiKey:
-          'AIzaSyA_UOaQBJrs7WP2Ex_pqYOq55DsuCX17Q8', // paste your api key here
+          'AIzaSyAvHZomr6E2-mcH2X-PS5h-vM0fPETK3d0', // paste your api key here
+      authDomain: 'sneak-boutique.firebaseapp.com',
       appId:
-          '1:609122838747:android:620178ca0b09da379ac4fe', //paste your app id here
+          '1:609122838747:web:e7875e8b45b0c3859ac4fe', //paste your app id here
       messagingSenderId: '609122838747', //paste your messagingSenderId here
       projectId: 'sneak-boutique', //paste your project id here
-      storageBucket: 'gs://sneak-boutique.appspot.com',
+      storageBucket: 'sneak-boutique.appspot.com',
     ),
   );
   Bloc.observer = SimpleBlocObserver();
