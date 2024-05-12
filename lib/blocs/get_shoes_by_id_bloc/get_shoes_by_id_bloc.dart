e0c2b8292a13bdd87780923a09deae1b487f54e2
GRAND Thomas <thomas1.grand@epitech.eu>
2024-05-12 23:27:51 +0200

    ajout du analyze

--- lib/blocs/get_shoes_by_id_bloc/get_shoes_by_id_bloc.dart
+++ lib/blocs/get_shoes_by_id_bloc/get_shoes_by_id_bloc.dart
@@ -5,18 +5,28 @@ import 'package:shoes_repository/shoes_repository.dart';
 part 'get_shoes_by_id_event.dart';
 part 'get_shoes_by_id_state.dart';
 
+/// Un bloc pour obtenir des chaussures par leur identifiant.
+///
+/// Ce bloc gère l'état de la récupération des chaussures en fonction de leur identifiant.
+/// Il prend en charge les événements pour récupérer les chaussures et met à jour l'état en conséquence.
 class GetShoesByIdBloc extends Bloc<GetShoesByIdEvent, GetShoesByIdState> {
+  /// Le constructeur pour initialiser le bloc avec le dépôt de chaussures spécifié.
+  ///
+  /// [shoesRepo] : Le dépôt de chaussures utilisé pour récupérer les données des chaussures.
   GetShoesByIdBloc(this.shoesRepo) : super(GetShoesByLoading()) {
     on<GetShoesById>((GetShoesById event, Emitter<GetShoesByIdState> emit) async {
       try {
+        // Tente de récupérer les chaussures en fonction de l'identifiant fourni dans l'événement.
         final Shoes shoes = await shoesRepo.getShoesById(event.id);
+        // Émet un nouvel état indiquant le succès de la récupération des chaussures.
         emit(GetShoesBySuccess(shoes));
       } catch (e) {
+        // En cas d'erreur lors de la récupération des chaussures, émet un état d'échec.
         emit(GetShoesByIdFailure());
       }
     });
   }
 
-  /// The repository for handling shoe-related data.
+  /// Le dépôt pour gérer les données liées aux chaussures.
   ShoesRepo shoesRepo;
 }
--- lib/blocs/get_shoes_by_id_bloc/get_shoes_by_id_event.dart
+++ lib/blocs/get_shoes_by_id_bloc/get_shoes_by_id_event.dart
@@ -1,17 +1,24 @@
 part of 'get_shoes_by_id_bloc.dart';
 
+/// Événement abstrait pour obtenir des chaussures par leur identifiant.
+///
+/// Les sous-classes de cet événement doivent implémenter la méthode [props]
+/// pour permettre la comparaison d'égalité entre les événements.
 abstract class GetShoesByIdEvent extends Equatable {
   const GetShoesByIdEvent();
 
   @override
-  List<Object?> get props => [];
+  List<Object?> get props => <Object?>[];
 }
 
+/// Événement pour obtenir des chaussures spécifiques par leur identifiant.
+///
+/// [id] : L'identifiant unique des chaussures à récupérer.
 class GetShoesById extends GetShoesByIdEvent {
-  final String id;
 
   const GetShoesById(this.id);
+  final String id;
 
   @override
-  List<Object?> get props => [id];
+  List<Object?> get props => <Object?>[id];
 }
