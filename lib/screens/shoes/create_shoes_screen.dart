36da92cc66d8c47e7a16cf3e99977afcf0a7cabd
GRAND Thomas <thomas1.grand@epitech.eu>
2024-05-12 15:46:50 +0200

    feat(KAN-28): refonte de la creation de chaussures

--- lib/screens/shoes/create_shoes_screen.dart
+++ lib/screens/shoes/create_shoes_screen.dart
@@ -59,203 +59,203 @@ class _CreateShoesScreenState extends State<CreateShoesScreen> {
         },
         child: Scaffold(
           appBar: AppBar(
-            backgroundColor: Theme.of(context).colorScheme.background,
+            backgroundColor: Colors.white,
           ),
-          backgroundColor: Theme.of(context).colorScheme.background,
-          body: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  const Text(
-                    'Create a New SHoes !',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  InkWell(
-                    borderRadius: BorderRadius.circular(20),
-                    onTap: () async {
-                      final ImagePicker picker = ImagePicker();
-                      final XFile? image = await picker.pickImage(
-                        source: ImageSource.gallery,
-                      );
-                      if (image != null && context.mounted) {
-                        context.read<UploadPictureBloc>().add(
-                              UploadPicture(
-                                await image.readAsBytes(),
-                                basename(image.path),
-                              ),
-                            );
-                      }
-                    },
-                    child: shoes.picture.isNotEmpty
-                        ? Container(
-                            width: 400,
-                            height: 400,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(20),
-                              image: DecorationImage(
-                                image: NetworkImage(shoes.picture),
-                                fit: BoxFit.contain,
-                              ),
+          backgroundColor: Colors.white,
+          body: Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: <Widget>[
+                    const Text(
+                      'Create a new pair of Shoes',
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    InkWell(
+                      borderRadius: BorderRadius.circular(20),
+                      onTap: () async {
+                        final ImagePicker picker = ImagePicker();
+                        final XFile? image = await picker.pickImage(
+                          source: ImageSource.gallery,
+                        );
+                        if (image != null && context.mounted) {
+                          context.read<UploadPictureBloc>().add(
+                            UploadPicture(
+                              await image.readAsBytes(),
+                              basename(image.path),
                             ),
-                          )
-                        : Ink(
-                            width: 400,
-                            height: 400,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(20),
-                              color: Colors.white,
+                          );
+                        }
+                      },
+                      child: shoes.picture.isNotEmpty
+                          ? Container(
+                        width: 400,
+                        height: 400,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          image: DecorationImage(
+                            image: NetworkImage(shoes.picture),
+                            fit: BoxFit.contain,
+                          ),
+                        ),
+                      )
+                          : Ink(
+                        width: 400,
+                        height: 400,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: const Color.fromARGB(255, 213, 213, 213),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Icon(
+                              CupertinoIcons.photo,
+                              size: 100,
+                              color: Colors.grey.shade200,
                             ),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            const Text(
+                              'Add a Picture here...',
+                              style: TextStyle(color: Colors.grey),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Form(
+                      key: _formKey,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          SizedBox(
+                            width: 400,
+                            child: MyTextField(
+                              controller: nameController,
+                              hintText: 'Name',
+                              obscureText: false,
+                              keyboardType: TextInputType.text,
+                              errorMsg: _errorMsg,
+                              validator: (String? val) {
+                                if (val!.isEmpty) {
+                                  return 'Please fill in this field';
+                                }
+                                return null;
+                              },
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          SizedBox(
+                            width: 400,
+                            child: MyTextField(
+                              controller: descriptionController,
+                              hintText: 'Description',
+                              obscureText: false,
+                              keyboardType: TextInputType.text,
+                              errorMsg: _errorMsg,
+                              validator: (String? val) {
+                                if (val!.isEmpty) {
+                                  return 'Please fill in this field';
+                                }
+                                return null;
+                              },
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          SizedBox(
+                            width: 400,
+                            child: Row(
                               children: <Widget>[
-                                Icon(
-                                  CupertinoIcons.photo,
-                                  size: 100,
-                                  color: Colors.grey.shade200,
-                                ),
-                                const SizedBox(
-                                  height: 10,
-                                ),
-                                const Text(
-                                  'Add a Picture here...',
-                                  style: TextStyle(color: Colors.grey),
+                                Expanded(
+                                  child: MyTextField(
+                                    controller: priceController,
+                                    hintText: 'Price',
+                                    obscureText: false,
+                                    keyboardType: TextInputType.number,
+                                    errorMsg: _errorMsg,
+                                    validator: (String? val) {
+                                      if (val!.isEmpty) {
+                                        return 'Please fill in this field';
+                                      }
+                                      return null;
+                                    },
+                                  ),
                                 ),
                               ],
                             ),
                           ),
-                  ),
-                  const SizedBox(height: 20),
-                  Form(
-                    key: _formKey,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        SizedBox(
-                          width: 400,
-                          child: MyTextField(
-                            controller: nameController,
-                            hintText: 'Name',
-                            obscureText: false,
-                            keyboardType: TextInputType.text,
-                            errorMsg: _errorMsg,
-                            validator: (String? val) {
-                              if (val!.isEmpty) {
-                                return 'Please fill in this field';
-                              }
-                              return null;
-                            },
-                          ),
-                        ),
-                        const SizedBox(height: 10),
-                        SizedBox(
-                          width: 400,
-                          child: MyTextField(
-                            controller: descriptionController,
-                            hintText: 'Description',
-                            obscureText: false,
-                            keyboardType: TextInputType.text,
-                            errorMsg: _errorMsg,
-                            validator: (String? val) {
-                              if (val!.isEmpty) {
-                                return 'Please fill in this field';
-                              }
-                              return null;
-                            },
-                          ),
-                        ),
-                        const SizedBox(height: 10),
-                        SizedBox(
-                          width: 400,
-                          child: Row(
-                            children: <Widget>[
-                              Expanded(
-                                child: MyTextField(
-                                  controller: priceController,
-                                  hintText: 'Price',
-                                  obscureText: false,
-                                  keyboardType: TextInputType.number,
-                                  errorMsg: _errorMsg,
-                                  validator: (String? val) {
-                                    if (val!.isEmpty) {
-                                      return 'Please fill in this field';
-                                    }
-                                    return null;
-                                  },
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  if (!creationRequired)
-                    SizedBox(
-                      width: 400,
-                      height: 40,
-                      child: TextButton(
-                        onPressed: () {
-                          if (_formKey.currentState!.validate()) {
-                            setState(() {
-                              shoes
-                                ..name = nameController.text
-                                ..description = descriptionController.text
-                                ..sizeShoes = <int>[
-                                  36,
-                                  37,
-                                  38,
-                                  39,
-                                  40,
-                                  41,
-                                  42,
-                                  43,
-                                  44,
-                                  45,
-                                ]
-                                ..price = double.parse(priceController.text);
-                            });
-                            context
-                                .read<CreateShoesBloc>()
-                                .add(CreateShoes(shoes));
-                          }
-                        },
-                        style: TextButton.styleFrom(
-                          elevation: 3,
-                          backgroundColor:
-                              Theme.of(context).colorScheme.primary,
-                          foregroundColor: Colors.white,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(60),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    if (!creationRequired)
+                      SizedBox(
+                        width: 400,
+                        height: 40,
+                        child: TextButton(
+                          onPressed: () {
+                            if (_formKey.currentState!.validate()) {
+                              setState(() {
+                                shoes
+                                  ..name = nameController.text
+                                  ..description = descriptionController.text
+                                  ..sizeShoes = <int>[
+                                    36,
+                                    37,
+                                    38,
+                                    39,
+                                    40,
+                                    41,
+                                    42,
+                                    43,
+                                    44,
+                                    45,
+                                  ]
+                                  ..price = double.parse(priceController.text);
+                              });
+                              context
+                                  .read<CreateShoesBloc>()
+                                  .add(CreateShoes(shoes));
+                            }
+                          },
+                          style: TextButton.styleFrom(
+                            elevation: 3,
+                            backgroundColor: Colors.black,
+                            foregroundColor: Colors.white,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(60),
+                            ),
                           ),
-                        ),
-                        child: const Padding(
-                          padding: EdgeInsets.symmetric(
-                            horizontal: 25,
-                            vertical: 5,
-                          ),
-                          child: Text(
-                            'Create Pizza',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 16,
-                              fontWeight: FontWeight.w600,
+                          child: const Padding(
+                            padding: EdgeInsets.symmetric(
+                              horizontal: 25,
+                              vertical: 5,
+                            ),
+                            child: Text(
+                              'Publish',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w600,
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                    )
-                  else
-                    const CircularProgressIndicator(),
-                ],
+                      )
+                    else
+                      const CircularProgressIndicator(),
+                  ],
+                ),
               ),
             ),
           ),
