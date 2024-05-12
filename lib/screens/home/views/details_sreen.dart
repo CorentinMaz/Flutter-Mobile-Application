39429eca6c817daa095e6dcacd8f1137a980e34b
GRAND Thomas <thomas1.grand@epitech.eu>
2024-05-12 09:51:12 +0200

    feat(KAN-24): modification front detail chaussure

--- lib/screens/home/views/details_sreen.dart
+++ lib/screens/home/views/details_sreen.dart
@@ -32,8 +32,8 @@ class DetailsScreenState extends State<DetailsScreen> {
           child: Column(
             children: <Widget>[
               Container(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.width - (40),
+                width: MediaQuery.of(context).size.width / 1.5,
+                height: MediaQuery.of(context).size.height / 2,
                 decoration: BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.circular(30),
@@ -54,119 +54,136 @@ class DetailsScreenState extends State<DetailsScreen> {
               const SizedBox(
                 height: 30,
               ),
-              DecoratedBox(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(30),
-                  boxShadow: const <BoxShadow>[
-                    BoxShadow(
-                      color: Colors.grey,
-                      offset: Offset(3, 3),
-                      blurRadius: 5,
+              Center(
+                child: Container(
+                  width: MediaQuery.of(context).size.width / 1.5,
+                  child: DecoratedBox(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(30),
+                      boxShadow: const <BoxShadow>[
+                        BoxShadow(
+                          color: Colors.grey,
+                          offset: Offset(3, 3),
+                          blurRadius: 5,
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(20),
-                  child: Column(
-                    children: <Widget>[
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          Expanded(
-                            flex: 2,
-                            child: Text(
-                              widget.shoes.name,
-                              style: const TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
+                    child: Center(
+                      child: Container(
+                        child: Padding(
+                          padding: const EdgeInsets.all(20),
+                          child: Column(
+                            children: <Widget>[
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: <Widget>[
+                                  Expanded(
+                                    flex: 2,
+                                    child: Text(
+                                      widget.shoes.name,
+                                      style: const TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
+                                  Expanded(
+                                    child: Align(
+                                      alignment: Alignment.centerRight,
+                                      child: Text(
+                                        '${widget.shoes.price}€',
+                                        style: const TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.black,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
                               ),
-                            ),
-                          ),
-                          Expanded(
-                            child: Align(
-                              alignment: Alignment.centerRight,
-                              child: Text(
-                                '${widget.shoes.price}€',
-                                style: TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
-                                  color: Theme.of(context).colorScheme.primary,
-                                ),
+                              const SizedBox(
+                                height: 12,
                               ),
-                            ),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(
-                        height: 12,
-                      ),
-                      Wrap(
-                        spacing: 10,
-                        runSpacing: 10,
-                        children: widget.shoes.sizeShoes.map((int size) {
-                          return GestureDetector(
-                            onTap: () {
-                              setState(() {
-                                selectedSize = size.toString();
-                              });
-                            },
-                            child: Padding(
-                              padding: const EdgeInsets.only(right: 10),
-                              child: Container(
-                                width: 40, // Largeur du carré
-                                height: 40, // Hauteur du carré
-                                decoration: BoxDecoration(
-                                  color: selectedSize == size.toString()
-                                      ? Colors.blue
-                                      : Colors.grey[200],
-                                  borderRadius: BorderRadius.circular(8),
-                                ),
-                                child: Center(
-                                  child: Text(
-                                    size.toString(),
-                                    style: const TextStyle(
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.bold,
+                              Wrap(
+                                spacing: 10,
+                                runSpacing: 10,
+                                children:
+                                    widget.shoes.sizeShoes.map((int size) {
+                                  return GestureDetector(
+                                    onTap: () {
+                                      setState(() {
+                                        selectedSize = size.toString();
+                                      });
+                                    },
+                                    child: Padding(
+                                      padding: const EdgeInsets.only(right: 10),
+                                      child: AnimatedContainer(
+                                        duration: Duration(milliseconds: 300),
+                                        width: 40, // Largeur du carré
+                                        height: 40, // Hauteur du carré
+                                        decoration: BoxDecoration(
+                                          color: selectedSize == size.toString()
+                                              ? Colors.black
+                                              : Colors.grey[200],
+                                          borderRadius:
+                                              BorderRadius.circular(8),
+                                        ),
+                                        child: Center(
+                                          child: Text(
+                                            size.toString(),
+                                            style: TextStyle(
+                                              fontSize: 16,
+                                              fontWeight: FontWeight.bold,
+                                              color: selectedSize ==
+                                                      size.toString()
+                                                  ? Colors.white
+                                                  : Colors.black,
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  );
+                                }).toList(),
+                              ),
+                              const SizedBox(
+                                height: 40,
+                              ),
+                              SizedBox(
+                                width: MediaQuery.of(context).size.width,
+                                height: 50,
+                                child: TextButton(
+                                  onPressed: () {
+                                    if (kDebugMode) {
+                                      print('Selected Size: $selectedSize');
+                                    }
+                                  },
+                                  style: TextButton.styleFrom(
+                                    elevation: 3,
+                                    backgroundColor: Colors.black,
+                                    foregroundColor: Colors.white,
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10),
+                                    ),
+                                  ),
+                                  child: const Text(
+                                    'Buy Now',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w600,
                                     ),
                                   ),
                                 ),
                               ),
-                            ),
-                          );
-                        }).toList(),
-                      ),
-                      const SizedBox(
-                        height: 40,
-                      ),
-                      SizedBox(
-                        width: MediaQuery.of(context).size.width,
-                        height: 50,
-                        child: TextButton(
-                          onPressed: () {
-                            if (kDebugMode) {
-                              print('Selected Size: $selectedSize');
-                            }
-                          },
-                          style: TextButton.styleFrom(
-                            elevation: 3,
-                            backgroundColor: Colors.black,
-                            foregroundColor: Colors.white,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                          ),
-                          child: const Text(
-                            'Buy Now',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20,
-                              fontWeight: FontWeight.w600,
-                            ),
+                            ],
                           ),
                         ),
                       ),
-                    ],
+                    ),
                   ),
                 ),
               ),
